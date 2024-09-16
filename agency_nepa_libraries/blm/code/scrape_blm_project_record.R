e42b81fe19caa0f887cd4ce9966e00dca41b9d75
Tyler Scott <tascott@ucdavis.edu>
2024-09-16 10:11:29 -0700

    current rselenium not working

--- agency_nepa_libraries/blm/code/scrape_blm_project_record.R
+++ agency_nepa_libraries/blm/code/scrape_blm_project_record.R
@@ -21,7 +21,7 @@ t2 <- fread(project_record_file2)
 
 if(file.exists(project_record_file)){precord = fread(project_record_file)}else{precord = data.table()}
 
-
+docker run -d -p 4445:4444 selenium/standalone-firefox
 
 remDr <- RSelenium::remoteDriver(remoteServerAddr = "localhost",
                                  port = 4445L,
